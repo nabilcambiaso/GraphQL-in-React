82148f8eb0d7e34fc81a7d7c4ed70d3f0efc1230
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-04-13 23:23:46 +0000

    Use mutation to edit data

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useQuery,useLazyQuery, gql } from '@apollo/client'
+import { useQuery,useLazyQuery, gql, useMutation } from '@apollo/client'
 
 const QUERY_ALL_USERS = gql`
 query getusers{
@@ -30,11 +30,28 @@ query Movie($name:String!) {
 }
 `
 
+const CREATE_USER_MUTATION = gql`
+mutation CreateUser($input:createUserInput!) {
+  createUser(input:$input) {
+  name
+  username
+  age 
+  nationality
+  }
+}
+`
+
 function DisplayData() {
     const [movieSearched,setMovieSearched] = useState("");
-    const { data, loading, error } = useQuery(QUERY_ALL_USERS)
+    const [name,setName] = useState("");
+    const [username,setUserName] = useState("");
+    const [age,setAge] = useState(0);
+    const [nationality,setNationality] = useState("");
+
+    const { data, loading, error,refetch } = useQuery(QUERY_ALL_USERS)
     const { data:moviesData } = useQuery(QUERY_ALL_MOVIES)
     const [fetchMovies, {data:movieData,error:movieError}] = useLazyQuery(QUERY_A_MOVIE)
+    const [createUser] = useMutation(CREATE_USER_MUTATION)
     if (loading) {
         <h1>Data is loading...</h1>
     }
@@ -49,8 +66,23 @@ function DisplayData() {
      
     return (
         <div>
-            <h1> Display Users</h1>
+              <input type="text" placeholder='name' onChange={(event)=>{setName(event.target.value)}}/>
+              <input type="text" placeholder=' usename' onChange={(event)=>{setUserName(event.target.value)}}/>
+              <input type="number" placeholder='age' onChange={(event)=>{setAge(Number(event.target.value))}}/>
+              <input type="text" placeholder='nationality' onChange={(event)=>{setNationality(event.target.value.toUpperCase())}}/>
 
+            <button onClick={()=>{
+                createUser({variables:{
+                    input:{
+                      name,
+                      username,
+                      age,
+                      nationality
+                    }
+                }})
+                refetch()
+            }}>Add User</button>
+            <h1> Display Users</h1>
             <div>{data && data.users.map((user) => (
                 <div style={{ marginBottom: 10, backgroundColor: "beige" }}>
                     <h3>name: {user.name}</h3>
